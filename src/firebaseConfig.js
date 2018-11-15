c239697afe88e6c28c16b4ee7fbf7d461e75c0b9
Nebera Anton <anebera@apextech.com.ua>
2018-11-15 02:03:35 -0700

    - moved private configs to json file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,24 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import settings from '../firebase'
 
-// firebase init goes here
-const config = {
-    apiKey: "AIzaSyAkA--bkjdlCUW3rgXdbF8BtYZbj73S3ZE",
-    authDomain: "my-awesome-project-id-13a4a.firebaseapp.com",
-    databaseURL: "https://my-awesome-project-id-13a4a.firebaseio.com",
-    projectId: "my-awesome-project-id-13a4a",
-    storageBucket: "my-awesome-project-id-13a4a.appspot.com",
-    messagingSenderId: "987287177047"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(settings.config);
 
 // firebase utils
 const db = firebase.firestore();
 const auth = firebase.auth();
 const currentUser = auth.currentUser;
 
-const settings = {timestampsInSnapshots: true};
-db.settings(settings);
+const defaultSettings = {timestampsInSnapshots: true};
+db.settings(defaultSettings);
 
 // firebase collections
 const productsCollection = db.collection('products');
